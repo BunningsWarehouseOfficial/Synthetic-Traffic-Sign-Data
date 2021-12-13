f750a4916f51ce7a1012f362407252200c11dec5
Allen <22706998@student.uwa.edu.au>
2021-12-13 15:30:06 +0800

    Added code to project a sequence of signs at varying distances on the image, overlaying over the same image
    Former-commit-id: 40d635fc5fafc92c87e11fedc9a0003434293014

--- signbreaker/create_sequences_auto.py
+++ signbreaker/create_sequences_auto.py
@@ -4,12 +4,12 @@ Extended from create_sequences.py
 Script for generating sequences of traffic sign detection training data. Imitates how a moving camera would capture
 multiple frames of a scene where the target object is at slightly different distances from the camera in each frame.
 
-The distance of the near and far anchor points are passed as command line arguments, with the anchors' coordinates
-and size being scaled by perspective projection, and intermediate frames being produced via linear interpolation. 
+The distance of the near and far anchor points are passed as command line arguments, and a set of foregrounds, with
+evenly distributed distances over the near and far anchor distances, are produced via perspective projection. 
+
 The camera's focal length or field of view is also passed as a command line argument to apply perspective projection.
 """
-# Author: Kristian Rados
-# Extended by: Allen Antony
+# Author: Kristian Rados, Allen Antony
 
 OUT_DIR         = "SGTS_Sequences"
 LABELS_FILE     = "labels.txt"
@@ -36,6 +36,10 @@ def parse_arguments():
 
 
 class Anchor(object):
+    """
+    A class that stores OpenCV pixel coordinates of the top left corner of the sign, 
+    as well as the size of the sign in pixels.
+    """
     def __init__(self, bg_size, NDC_x, NDC_y, x_size, y_size):
         height, width, _ = bg_size
         x_size, y_size = x_size / 2, y_size / 2
@@ -59,6 +63,10 @@ class Anchor(object):
       
         
 class SignObject(object):
+    """
+    A class that stores the world coordinates of a sign object, applying perspective projection via
+    the method 'perspective_transform'
+    """
     def __init__(self, x, y, z, size):
         # Top left corner of sign
         self.x1 = x 
@@ -71,8 +79,8 @@ class SignObject(object):
         
     def perspective_transform(self, bg_size, proj_matrix):
         """
-        Applies perspective projection to the sign object, and returns the coordinates of the top-left and bottom-right
-        corners of the sign object in the image.
+        Applies perspective projection to the sign object to produce normalised device coordinates (NDC), 
+        and initialises and returns an anchor object using these new coordinates.
         """
         # Scale the sign coordinates by the perspective projection matrix
         x1, y1, _, w = proj_matrix.dot(np.array([self.x1, self.y1, self.z, 1]))
@@ -95,6 +103,11 @@ def dir_path(path):
     
     
 def create_perspective(fovy, aspect, near, far):
+    """[summary]
+    Creates a perspective projection matrix using vertical fov, aspect ratio, and near and
+    far clipping distances. 
+    Code adapted from http://learnwebgl.brown37.net/08_projections/projections_perspective.html
+    """
     if (fovy <= 0 or fovy >= 180 or aspect <= 0 or near >= far or near <= 0):
         raise ValueError('Invalid parameters to createPerspective')
     half_fovy = math.radians(fovy) / 2
@@ -107,6 +120,12 @@ def create_perspective(fovy, aspect, near, far):
 
 
 def create_frustrum(left, right, bottom, top, near, far):
+    """[summary]
+    Creates a perspective projection matrix using the left, right, bottom, top, near and far
+    bounds of the near clipping plane.
+    Code adapted from http://learnwebgl.brown37.net/08_projections/projections_perspective.html
+    """
+    # http://learnwebgl.brown37.net/08_projections/projections_perspective.html
     if (left == right or bottom == top or near == far):
         raise ValueError('Invalid parameters to createFrustrum')
     if (near <= 0 or far <= 0):
@@ -126,14 +145,18 @@ def create_frustrum(left, right, bottom, top, near, far):
     
 
 def produce_anchors(bg_size, size, x, y, min_dist, max_dist, num_frames):
+    """[summary]
+    Generates a list of anchor objects, by applying perspective projection on the constant
+    world coordinates, depending on virtual distance from the camera. Using np.linspace, the 
+    distances are evenly distributed over min_dist to max_dist to create the anchor objects.
+    """
     anchors = []
     height, width, _ = bg_size
     aspect_ratio = width / height
     proj_matrix = create_perspective(FOVY, aspect_ratio, NEAR_CLIPPING_PLANE_DIST, FAR_CLIPPING_PLANE_DIST)
     
-    # Projection matrix assumes negative z values in front of camera
-    for z in np.linspace(min_dist, max_dist, num=num_frames, endpoint=True):
-        sign_z = -1 * z
+    for dist in np.linspace(max_dist, min_dist, num=num_frames, endpoint=True):
+        sign_z = -1 * dist      # Projection matrix assumes negative z values in front of camera
         sign_near = SignObject(x, y, z=sign_z, size=size)
         anchor = sign_near.perspective_transform(bg_size, proj_matrix)
         anchors.append(anchor)
@@ -173,63 +196,71 @@ def main():
   
   
 # World coordinates for sign objects
-sign_coords = {'x':1.5, 'y':1, 'size':0.5}
+SIGN_COORDS = {'x':1.5, 'y':1, 'size':0.5}
 
 
-def get_view_plane_bounds(distance, fovy, aspect_ratio):
-    top = distance * math.tan(math.radians(fovy) / 2)
-    right = top * aspect_ratio
-    left = -right
-    bottom = -top
-    return {'top':top, 'right':right, 'left':left, 'bottom':bottom}
+class ShowAnchors(object):
+    def __init__(self, bg_path, fg_path, min_dist, max_dist, num_frames):
+        self.bg_img = cv2.imread(bg_path)
+        self.fg_img = cv2.imread(fg_path)
+        self.display_img = self.bg_img.copy()
+        height, width, _ = self.bg_img.shape
+        self.aspect_ratio = width / height
+        self.min_dist = min_dist
+        self.max_dist = max_dist
+        self.num_frames = num_frames
+        
+        self.x_trackbar_max = 200
+        self.y_trackbar_max = 200
+        
+        self.draw_anchors(SIGN_COORDS['size'], SIGN_COORDS['x'], SIGN_COORDS['y'], self.min_dist, self.max_dist,
+                          self.num_frames)
+        cv2.createTrackbar('x', 'image', 0, self.x_trackbar_max, self.x_on_change)
+        cv2.createTrackbar('y', 'image', 0, self.y_trackbar_max, self.y_on_change)
+        cv2.waitKey(0)
+        
+
+    def get_view_plane_bounds(self, distance, fovy, aspect_ratio):
+        top = distance * math.tan(math.radians(fovy) / 2)
+        right = top * aspect_ratio
+        left = -right
+        bottom = -top
+        return {'top':top, 'right':right, 'left':left, 'bottom':bottom}
 
 
-def draw_anchors(sign_path, bg_path, fg_size, x, y, min_dist, max_dist, num_frames):
-    sign_img = cv2.imread(sign_path)
-    bg_img = cv2.imread(bg_path)
-    
-    anchors = produce_anchors(bg_img.shape, fg_size, x, y, min_dist, max_dist, num_frames)
-    
-    for anchor in anchors:
-        sign_img_scaled = cv2.resize(sign_img, (anchor.size, anchor.size))
-        bg_img = overlay(sign_img_scaled, bg_img, anchor.screen_x, anchor.screen_y)
-    cv2.imshow('image', bg_img)
-    
-    
-def x_on_change(val):
-    x_prop = 2 * val / X_TRACKBAR_MAX - 1
-    right_bound = get_view_plane_bounds(args.min_dist, FOVY, aspect_ratio)['right']
-    sign_coords['x'] = x_prop * right_bound
-    draw_anchors(sign_path, bg_path, sign_coords['size'], sign_coords['x'], sign_coords['y'], 
-                args.min_dist, args.max_dist, args.num_frames)
-    
-    
-def y_on_change(val):
-    y_prop = 2 * val / Y_TRACKBAR_MAX - 1
-    upper_bound = get_view_plane_bounds(args.min_dist, FOVY, aspect_ratio)['top']
-    sign_coords['y'] = y_prop * upper_bound
-    draw_anchors(sign_path, bg_path, sign_coords['size'], sign_coords['x'], sign_coords['y'], 
-                args.min_dist, args.max_dist, args.num_frames)   
+    def draw_anchors(self, fg_size, x, y, min_dist, max_dist, num_frames):
+        self.display_img = self.bg_img.copy()
+        anchors = produce_anchors(self.bg_img.shape, fg_size, x, y, min_dist, max_dist, num_frames)
+        
+        for anchor in anchors:
+            sign_img_scaled = cv2.resize(self.fg_img, (anchor.size, anchor.size))
+            self.display_img = overlay(sign_img_scaled, self.display_img, anchor.screen_x, anchor.screen_y)
+        cv2.imshow('image', self.display_img)
+        
+        
+    def x_on_change(self, val):
+        x_prop = 2 * val / self.x_trackbar_max - 1
+        right_bound = self.get_view_plane_bounds(args.min_dist, FOVY, self.aspect_ratio)['right']
+        SIGN_COORDS['x'] = x_prop * right_bound
+        self.draw_anchors(SIGN_COORDS['size'], SIGN_COORDS['x'], SIGN_COORDS['y'], 
+                        self.min_dist, self.max_dist, self.num_frames)
+        
+        
+    def y_on_change(self, val):
+        y_prop = 2 * val / self.y_trackbar_max - 1
+        upper_bound = self.get_view_plane_bounds(args.min_dist, FOVY, self.aspect_ratio)['top']
+        SIGN_COORDS['y'] = y_prop * upper_bound
+        self.draw_anchors(SIGN_COORDS['size'], SIGN_COORDS['x'], SIGN_COORDS['y'], 
+                        self.min_dist, self.max_dist, self.num_frames)   
 
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 bg_path = os.path.join(current_dir, 'Backgrounds/GTSDB/00049.png')
 sign_path = os.path.join(current_dir, 'Signs/0.jpg')
-img = cv2.imread(bg_path)
-height, width, channels = img.shape
-aspect_ratio = width / height
 args = parse_arguments()
 
 NEAR_CLIPPING_PLANE_DIST = 2
 FAR_CLIPPING_PLANE_DIST = 50
-FOVY = args.fovy            
+FOVY = args.fovy
 
-cv2.imshow('image', img)
-
-X_TRACKBAR_MAX = 200
-Y_TRACKBAR_MAX = 200
-
-cv2.createTrackbar('x', 'image', 0, X_TRACKBAR_MAX, x_on_change)
-cv2.createTrackbar('y', 'image', 0, Y_TRACKBAR_MAX, y_on_change)
-
-cv2.waitKey(0)
+signs_viewer = ShowAnchors(bg_path, sign_path, args.min_dist, args.max_dist, args.num_frames)           
