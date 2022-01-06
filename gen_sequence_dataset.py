8d3c5192092cdd2c67040a2857f02bedcaed1ef5
Allen <22706998@student.uwa.edu.au>
2022-01-07 03:28:23 +0800

    Added sign distance to annotations, added numpy array creation and transformation of dataset into single labels within the dataset generation module
    Former-commit-id: d564661e73d7e3064783df45f254019b7810aca2

--- gen_sequence_dataset.py
+++ gen_sequence_dataset.py
@@ -31,7 +31,8 @@ def initialise_annotations(num_classes):
         "version": "1",
         "description": "Dataset of sequences of synthetically generated damaged signs",
         "contributor": "Curtin University",
-        "date_created": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        "date_created": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+        "frames_per_sequence": args.num_frames
     }
     annotations["licenses"] = {
         "id": 1,
@@ -58,13 +59,44 @@ def initialise_annotations(num_classes):
     return annotations
 
 
+def convert_to_single_label(dataset_path, original_annotations, new_annotations):
+    with open(os.path.join(dataset_path, original_annotations), 'r') as a_file:
+        a_json = json.load(a_file)
+        
+        a_json["categories"] = [
+            {
+                "id": 0,
+                "name": "signs",
+                "supercategory": "none"
+            },
+            {
+                "id": 1,
+                "name": "traffic_sign",
+                "supercategory": "signs"
+            }
+        ]
+        
+        for annotation in a_json['annotations']:
+            annotation['category_id'] = 1
+        
+        with open(os.path.join(dataset_path, new_annotations), 'w') as f:
+            json.dump(a_json, f, indent=4)
+        
+        # Create a npy file to store ground truths, for more efficient evaluation    
+        annotations_array = np.array([[a["image_id"], a["bbox"][0], a["bbox"][1], a["bbox"][2], a["bbox"][3], a["damage"], a["distance"], a["category_id"]] 
+                                    for a in a_json['annotations']])
+        
+        with open(os.path.join(dataset_path, '_single_annotations_array.npy'), 'wb') as f:
+            np.save(f, annotations_array)
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     
-    OUTDIR = args.out_dir
-    if os.path.exists(OUTDIR):
-        shutil.rmtree(OUTDIR)
-    os.mkdir(OUTDIR)
+    outdir = args.out_dir
+    if os.path.exists(outdir):
+        shutil.rmtree(outdir)
+    os.mkdir(outdir)
         
     annotations = initialise_annotations(len(os.listdir(args.original_sign_dir)))
     sign_paths = glob.glob(args.damaged_sign_dir + '/*/*.png')
@@ -84,9 +116,10 @@ if __name__ == '__main__':
             
             sign_img = cv2.imread(sign_path, cv2.IMREAD_UNCHANGED)
             original_sign_img = cv2.imread(original_sign_path, cv2.IMREAD_UNCHANGED)
-            image_paths, bounding_boxes = create_sequence(bg_img, sign_img, bg_name, sign_name, image_id, OUTDIR, num_frames=args.num_frames)
+            image_paths, bounding_boxes = create_sequence(bg_img, sign_img, bg_name, sign_name, image_id, outdir, num_frames=args.num_frames)
             
             for i in range(len(image_paths)):
+                # Format [x, y, width, height, distance]
                 bbox = bounding_boxes[i]
                 image_i = cv2.resize(sign_img, (bbox[2], bbox[3]))
                 original_image_i = cv2.resize(original_sign_img, (bbox[2], bbox[3]))
@@ -105,18 +138,20 @@ if __name__ == '__main__':
                         "id": image_id,        # one annotation per image
                         "image_id": image_id,
                         "category_id": int(cls_name) + 1,
-                        "bbox": bounding_boxes[i],
+                        "bbox": bbox,
                         "iscrowd": 0,
-                        "area": bounding_boxes[i][2] ** 2,
+                        "area": bbox[2] ** 2,
                         "segmentation": [],
-                        "damage": damage_i
+                        "damage": damage_i,
+                        "distance": bbox[-1]
                     }
                 )
                 image_id += 1
                 
-    annotations_path = os.path.join(OUTDIR, "_annotations.coco.json")
+    annotations_path = os.path.join(outdir, "_annotations.coco.json")
     with open(annotations_path, 'w') as f:
         json.dump(annotations, f, indent=4)
+    convert_to_single_label(outdir, '_annotations.coco.json', '_single_annotations.coco.json')
             
             
         
