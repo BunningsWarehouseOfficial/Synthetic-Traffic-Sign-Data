6468c1bc42d47c2efbba906695b3a135507365ba
Allen <22706998@student.uwa.edu.au>
2022-01-18 21:11:41 +0800

    feat: plot generated dfs as multiple lines in a single figure
    Former-commit-id: 0fdbf4cdaf2edd0f07fd80df7ecf2f2d360062e6

--- experiments/augment_experiment.py
+++ experiments/augment_experiment.py
@@ -2,24 +2,20 @@ import os
 import argparse
 import json
 import numpy as np
-import pandas as pd
-import plotly.express as px
-
-from collections import defaultdict
-from experiments.detection_eval import BoundingBox, get_pascal_voc_metrics, Box
+import plotly.graph_objects as go
 from tqdm import tqdm
 
 from experiments.damage_experiment import damage_experiment, distance_experiment
 
-
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--gt_file', default='/home/allenator/Pawsey-Internship/datasets/sgts_sequences/_single_annotations_array.npy', 
+parser.add_argument('--gt_file', default='/home/allenator/Pawsey-Internship/datasets/sgts_sequences_8/_single_annotations_array.npy', 
                     help='Ground truth annotations for dataset as a numpy file')
-parser.add_argument('--eval_files', default='/home/allenator/Pawsey-Internship/eval_dir/sgts_sequences_8/augmented.json',
+parser.add_argument('--eval_files', default='/home/allenator/Pawsey-Internship/eval_dir/sgts_sequences_8/augments.json',
                     help='Json of augment_level:file_path pairs')
 parser.add_argument('--num_frames', default=8, type=int, help='Number of frames per sequence the dataset')
+parser.add_argument('--experiment', default='damage', choices=['damage', 'distance'] , help='Type of experiment to evaluate')
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -30,13 +26,22 @@ if __name__ == "__main__":
     
     augment_dict = {}
     
+    # get data frames
     for aug in tqdm(eval_json):
         npy_path = eval_json[aug]
         pred_array = np.array(np.load(npy_path), dtype=np.float32)
         augment_dict[aug] = \
             {
-            'damage_experiment': damage_experiment(gt_array, pred_array),
-            'distance_experiment': distance_experiment(gt_array, pred_array)
+            'damage': damage_experiment(gt_array, pred_array, args.num_frames),
+            'distance': distance_experiment(gt_array, pred_array)
             }
+            
+    # plot given experiment
+    fig = go.Figure()
+    for aug in augment_dict:
+        df = augment_dict[aug][args.experiment]
+        fig = fig.add_trace(go.Scatter(x = df[args.experiment.capitalize()], y = df['mAP'], 
+                                       name=aug, mode='lines+markers'))
+    fig.show()
     
             
