3d18e0d0c1c7530915efba552c6acb02425bb0f8
Prasi21 <prasiasokan@gmail.com>
2022-05-09 16:04:21 +0800

    feat: added image rotation
    Former-commit-id: 86848ec59420d60a8582de34fc20aa9cbddabdf2

--- signbreaker/generate.py
+++ signbreaker/generate.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import imutils
 import cv2
 import random
+import numpy as np
 
 from synth_image import SynthImage
 
@@ -85,6 +86,16 @@ def new_data(synth_image):
         new_size = synth_image.fg_size
     else:
         x, y, new_size = SynthImage.gen_sign_coords(bg.shape[:2], fg.shape[:2])
+
+    pad = 40 # pixels to pad on each side of the image
+    fg = cv2.copyMakeBorder(fg, pad, pad, pad, pad, 0)
+    if(np.random.randint(10) < 10): # 50% chance of rotating
+        angle = int(np.random.normal(0,1)*180) # Normal distribution of rotation angle
+        # https://stackoverflow.com/questions/9041681/opencv-python-rotate-image-by-x-degrees-around-specific-point  
+        image_center = tuple(np.array(fg.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        fg = cv2.warpAffine(fg, rot_mat, fg.shape[1::-1], flags=cv2.INTER_LINEAR)
+
     fg = cv2.resize(fg, (new_size, new_size))
 
     axes = __bounding_axes(fg)  # Retrieve bounding axes of the sign image
