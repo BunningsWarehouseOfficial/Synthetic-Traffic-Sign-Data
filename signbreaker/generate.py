fix: Integrated multi signs as part of the standard code and not an extra flag
Instead of separate functions for handling multiple signs or not
We just have one for place any amount of signs (one or more)


Former-commit-id: b7fab1969a9ca384b824c531dc76be981f886824

signbreaker/generate.py
@@ -7,7 +7,6 @@ import imutils
 import cv2
 import random
 import numpy as np
-
 from synth_image import SynthImage
 
 def __has_opaque_pixel(line):
@@ -74,63 +73,25 @@ def __bounding_axes(img):
 
     return [x_left, x_right, y_top, y_bottom]
 
-def new_data(synth_image, online=False):
-    """Blends a synthetic sign with its corresponding background."""
-    bg_path = synth_image.bg_path
+
+def new_data(synth_image_set, online=False):
+    """Blends a set of synthetic signs with the corresponding background."""
+    bg_path = synth_image_set[0].bg_path
     bg = cv2.imread(bg_path, cv2.IMREAD_UNCHANGED)
     assert bg is not None, "Background image not found"
-
-    fg_path = synth_image.fg_path
-    if online is True:
-        fg = synth_image.fg_image
-    else:
-        fg = cv2.imread(fg_path, cv2.IMREAD_UNCHANGED)
-    assert fg is not None, "Foreground image not found"
-
-    if synth_image.fg_coords is not None and synth_image.fg_size is not None:
-        x, y = synth_image.fg_coords
-        new_size = synth_image.fg_size
-    else:
-        x, y, new_size = SynthImage.gen_sign_coords(bg.shape[:2], fg.shape[:2])
-
-    # pad = 40  # pixels to pad on each side of the image
-    # fg = cv2.copyMakeBorder(fg, pad, pad, pad, pad, cv2.BORDER_CONSTANT)
-    # if(np.random.randint(5) < 10):  # 50% chance of rotating
-    #     angle = int(np.random.normal(0,0.5)*180)  # Normal distribution of rotation angle
-    #     # https://stackoverflow.com/questions/9041681/opencv-python-rotate-image-by-x-degrees-around-specific-point  
-    #     image_center = tuple(np.array(fg.shape[1::-1]) / 2)
-    #     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    #     fg = cv2.warpAffine(fg, rot_mat, fg.shape[1::-1], flags=cv2.INTER_LINEAR)
-
-    image = overlay_new(fg, bg, new_size, x, y)
-    fg = cv2.resize(fg, (new_size, new_size))
-    axes = __bounding_axes(fg)  # Retrieve bounding axes of the sign image
-    axes[0] += x  # Adjusting bounding axis to make it relative to the whole bg image
-    axes[1] += x
-    axes[2] += y
-    axes[3] += y
-    synth_image.bounding_axes = axes
-    return image
-
-
-def new_data_multi(synth_images, online=False):
-    """Blends a synthetic sign with its corresponding background."""
-    bg_path = synth_images[0].bg_path
-    bg = cv2.imread(bg_path, cv2.IMREAD_UNCHANGED)
-    assert bg is not None, "Background image not found"
-
-    bg_h, bg_w, _ = bg.shape
     
     bboxes = []
-
     total = 0   # signs placed so far
-    nb_signs_in_img = len(synth_images)
+    nb_signs_in_img = len(synth_image_set)  # Total number of signs attempting place
     fail = 0    # attempts to place signs
 
     while total < nb_signs_in_img and fail < 40:
-        synth_image = synth_images[total]
-        fg_path = synth_image.fg_path
-        fg = cv2.imread(fg_path, cv2.IMREAD_UNCHANGED)
+        synth_image = synth_image_set[total]
+        if online is True:
+            fg = synth_image.fg_image
+        else:
+            fg_path = synth_image.fg_path
+            fg = cv2.imread(fg_path, cv2.IMREAD_UNCHANGED)
         assert fg is not None, "Foreground image not found"
         
         if synth_image.fg_coords is not None and synth_image.fg_size is not None:
@@ -152,8 +113,6 @@ def new_data_multi(synth_images, online=False):
             synth_image.bounding_axes = axes
         else:
             fail += 1
-
-    # TODO online support
 
     image = bg
     return image, total