fix: minor+WIP rotation fixes and TODOs
Former-commit-id: 89ba6536668b53a51ddf244a4d128eb9df23a4c4

signbreaker/generate.py
@@ -1,13 +1,21 @@
 """Module of functions for generating a synthetic manipulated sign dataset."""
 
 import os
-from utils import load_paths, dir_split, overlay, overlay_new
 from datetime import datetime
+import random
+import yaml
+
 import imutils
 import cv2
-import random
 import numpy as np
+
+from utils import load_paths, dir_split, overlay, overlay_new
 from synth_image import SynthImage
+
+# Open config file
+with open("config.yaml", "r") as ymlfile:
+    config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
 
 def __has_opaque_pixel(line):
     """Checks if a line of pixels contains a pixel above a transparency threshold."""
@@ -19,7 +27,7 @@ def __has_opaque_pixel(line):
             break  # Stop searching if one is found
     return opaque
 
-def __bounding_axes(img):
+def bounding_axes(img):  # TODO: Attempt speedup by making this function a regionprops wrapper
     """Returns the bounding axes of an image with a transparent background."""
     # Top axis
     y_top = 0
@@ -79,11 +87,6 @@ def new_data(synth_image_set, online=False):
     bg_path = synth_image_set[0].bg_path
     bg = cv2.imread(bg_path, cv2.IMREAD_UNCHANGED)
     assert bg is not None, "Background image not found"
-    
-    bboxes = []
-    total = 0   # signs placed so far
-    nb_signs_in_img = len(synth_image_set)  # Total number of signs attempting place
-    fail = 0    # attempts to place signs
 
     def get_fg(synth_image, online):
         if online is True:
@@ -95,10 +98,9 @@ def new_data(synth_image_set, online=False):
         return fg
 
     bboxes = []
-    total = 0   # signs placed so far
-    nb_signs_in_img = len(synth_image_set)  # Total number of signs attempting place
-    fail = 0    # attempts to place signs
-
+    total = 0   # Signs placed so far
+    nb_signs_in_img = len(synth_image_set)  # Total number of signs attempting to place
+    fail = 0    # Attempts to place signs
     while total < nb_signs_in_img and fail < 40:
         synth_image = synth_image_set[total]
         fg = get_fg(synth_image, online)
@@ -114,16 +116,18 @@ def new_data(synth_image_set, online=False):
             bboxes.append(bbox)
             total += 1
             fg = cv2.resize(fg, (new_size, new_size))
-            axes = __bounding_axes(fg)  # Retrieve bounding axes of the sign image
+            axes = bounding_axes(fg)  # Retrieve bounding axes of the sign image
             axes[0] += x  # Adjusting bounding axis to make it relative to the whole bg image
             axes[1] += x
             axes[2] += y
             axes[3] += y
             synth_image.bounding_axes = axes
-            do_place_below = True
+            do_place_below = config['vertical_grid_placement']
         else:
             fail += 1
 
+        # Place signs side-by-side in grid pattern
+        # TODO: Add horizontal placement, currently only does vertical placement
         while do_place_below and total < nb_signs_in_img and fail < 40:
             do_place_below = np.random.choice([True]*5 + [False]) and total < nb_signs_in_img
             if not (do_place_below and total < nb_signs_in_img and bbox):
@@ -141,7 +145,7 @@ def new_data(synth_image_set, online=False):
                 bboxes.append(bbox)
                 total += 1
                 fg = cv2.resize(fg, (new_size, new_size))
-                axes = __bounding_axes(fg)  # Retrieve bounding axes of the sign image
+                axes = bounding_axes(fg)  # Retrieve bounding axes of the sign image
                 axes[0] += x  # Adjusting bounding axis to make it relative to the whole bg image
                 axes[1] += x
                 axes[2] += y
@@ -149,13 +153,12 @@ def new_data(synth_image_set, online=False):
                 synth_image.bounding_axes = axes
             else:
                 fail += 1
-        
 
     image = bg
     return image, total
 
 
-#TODO: These two functions should be one function always include background using classes?
+# TODO: These two functions should be one function always include background using classes?
 # List of paths for all SGTSD relevant files using exposure_manipulation
 def paths_list(imgs_directory, bg_directory):
     directories = []
@@ -170,7 +173,7 @@ def paths_list(imgs_directory, bg_directory):
     return directories  # Directory for every single FILE and it's relevant bg FILE
 
 # List of paths for all SGTSD relevant files using fade_manipulation; backgrounds are assigned to 
-def assigned_paths_list(imgs_directory, bg_directory):  #TODO: is this the same as above?
+def assigned_paths_list(imgs_directory, bg_directory):  # TODO: Is this the same as above?
     directories = []
     for places in load_paths(bg_directory):  # Folder for each place: eg. GTSDB
         for imgs in load_paths(places):  # Iterate through each b.g. image: eg. IMG_0

signbreaker/manipulate.py
@@ -3,20 +3,24 @@
 # (https://github.com/alexandrosstergiou/Traffic-Sign-Recognition-basd-on-Synthesised-Training-Data)
 
 from abc import ABC, abstractmethod
-import cv2
-import numpy as np
 import ntpath
 import os
 import math
-from utils import load_paths, dir_split, get_truncated_normal, add_pole
-from PIL import Image, ImageStat, ImageEnhance
 import random
-from synth_image import SynthImage
-
-# Open and validate config file
 import yaml
+
+import cv2
+import numpy as np
+from PIL import Image, ImageStat, ImageEnhance
+
+from utils import load_paths, dir_split, get_truncated_normal, add_pole, scale_img
+from synth_image import SynthImage
+from generate import bounding_axes
+
+# Open config file
 with open("config.yaml", "r") as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
 
 ##################################
 ###  TRANSFORMATION FUNCTIONS  ###
@@ -33,6 +37,7 @@ class AbstractTransform(ABC):
 
         self.num_transformed = 0
         tform_imgs = []  # Collection of transformed images
+        tform_imgs.append(self.blank_transformation(img))
         for ii in range(num_transform):
             tform_img, descriptor = self.transformation(img)
             tform_imgs.append((tform_img, descriptor))
@@ -72,6 +77,11 @@ class AbstractTransform(ABC):
                 transformed_images.append(transformed_image)
         return transformed_images
 
+    def blank_transformation(self, img):
+        """Return the original image."""
+        # self.num_transformed += 1
+        return img, "original"
+
     @abstractmethod
     def transformation(self, img):
         pass
@@ -101,6 +111,11 @@ class RotationTransform(AbstractTransform):
         dz *= 2
 
         dest = self.rotate_image(img, angle[0], angle[1], angle[2], 0, 0, dz, f)
+        # Resize the image to the original size
+        # x_left, x_right, y_top, y_bottom = bounding_axes(dest)
+        # dest = dest[y_top:y_bottom+1, x_left:x_right+1]  # Crop blank edges
+        # dest_pil = scale_img(Image.fromarray(cv2.cvtColor(dest, cv2.COLOR_BGRA2RGBA)), config['sign_width'])
+        # dest = cv2.cvtColor(np.array(dest_pil), cv2.COLOR_RGBA2BGRA)
         self.num_transformed += 1
         return dest, f"{angle[0]:.2f}_{angle[1]:.2f}_{angle[2]:.2f}"
         
@@ -187,8 +202,8 @@ class FixedAffineTransform(AbstractTransform):
 
         # Transform function names are numbered in order of (my subjective) significance in visual difference
         #[0] 0 FORWARD FACING
-        def t0():
-            return img
+        # def t0():
+        #     return img
 
         #[3] 1 EAST FACING
         def t3():
@@ -320,7 +335,7 @@ class FixedAffineTransform(AbstractTransform):
             return cv2.warpAffine(img,M,(width,height))
 
         # Apply the number of transformations desired
-        transforms = [t0,t1,t2,t3,t4,t5,t6,t8,t9,t12,t13,t15]
+        transforms = [t1,t2,t3,t4,t5,t6,t8,t9,t12,t13,t15]
         if self.num_transformed == len(transforms):
             raise NotImplementedError(f"Only {len(transforms)} fixed affine transformations are currently implemented, but more were requested")
 